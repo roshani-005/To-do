1f334033c54af7f25df94f1c527f86f13c30dc17
Roshani <yroshani270@gmail.com>
2025-06-24 18:03:42 +0530

    Add files via upload

---
+++ Addtodoitem1.jsx
@@ -0,0 +1,20 @@
+function Todoitem1() {
+  return (
+    <div class="container text-center">
+      <div class="row kg-row">
+        <div class="col-6">Go To college</div>
+        <div class="col-4">04/08/2025</div>
+        <div class="col-2">
+          <button
+            type="button"
+            class="btn btn-danger
+          kg-button"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Todoitem1;
