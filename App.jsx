4cd7379d3abe403d886d5a094f22a8a2e8a1f4f8
Roshani <yroshani270@gmail.com>
2025-06-24 17:44:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+ import Hello from "./hello";
+ import Random from "./random";
+ 
+function App() {
+  return <div>
+    <h1>
+      This is the best react course
+    </h1>
+    <Hello></Hello>
+    <Random></Random>
+    
+  </div>
+}
+
+export default App;
