5c2e7dc6bdb9486372ec709f498c9d8b29748a5f
Roshani <yroshani270@gmail.com>
2025-06-24 18:04:32 +0530

    Add files via upload

---
+++ Appname.jsx
@@ -0,0 +1,9 @@
+function Appname() {
+  return (
+    <div>
+      <h1 className="head">TO-DO App</h1>
+      <p className="task">Enter Your Task Here....</p>
+    </div>
+  );
+}
+export default Appname;
